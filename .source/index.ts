cac6d33c7d981b53b38ca2f9ea845dab387e7fcf
p.phonsing_ <aprilpollo.apl@gmail.com>
2025-09-26 10:37:59 +0700

    # Intelligent Transportation Systems

--- .source/index.ts
+++ .source/index.ts
@@ -1,6 +1,7 @@
 // @ts-nocheck -- skip type checking
-import * as blog_1 from "../content/blog/rest-api.mdx?collection=blog&hash=1756031539907"
-import * as blog_0 from "../content/blog/auth.mdx?collection=blog&hash=1756031539907"
+import * as blog_2 from "../content/blog/rest-api.mdx?collection=blog&hash=1758788616636"
+import * as blog_1 from "../content/blog/intelligent-transport-system.mdx?collection=blog&hash=1758788616636"
+import * as blog_0 from "../content/blog/auth.mdx?collection=blog&hash=1758788616636"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
-export const blog = _runtime.docs<typeof _source.blog>([{ info: {"path":"auth.mdx","absolutePath":"/Users/apl/Documents/GitHub/aprilpollo/content/blog/auth.mdx"}, data: blog_0 }, { info: {"path":"rest-api.mdx","absolutePath":"/Users/apl/Documents/GitHub/aprilpollo/content/blog/rest-api.mdx"}, data: blog_1 }], [])
+export const blog = _runtime.docs<typeof _source.blog>([{ info: {"path":"auth.mdx","absolutePath":"/Users/aprilpollo/Documents/Github/aprilpollo/content/blog/auth.mdx"}, data: blog_0 }, { info: {"path":"intelligent-transport-system.mdx","absolutePath":"/Users/aprilpollo/Documents/Github/aprilpollo/content/blog/intelligent-transport-system.mdx"}, data: blog_1 }, { info: {"path":"rest-api.mdx","absolutePath":"/Users/aprilpollo/Documents/Github/aprilpollo/content/blog/rest-api.mdx"}, data: blog_2 }], [])
